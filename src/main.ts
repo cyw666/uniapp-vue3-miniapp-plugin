339c0c1843db2a3b4461fc9b912b505cfc1890e7
cyw <cyw19931210@163.com>
2023-10-11 17:43:57 +0800

    fix:build minapp plugin success

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
-import { createSSRApp } from 'vue';
-import App from './App.vue';
-import 'uno.css';
-
+import { createSSRApp } from 'vue'
+import App from './App.vue'
+import 'uno.css'
 export function createApp() {
-    const app = createSSRApp(App);
-
-    Object.values(import.meta.globEager('./modules/*.ts')).forEach(m => m.install?.(app));
-
-    return {
-        app,
-    };
+  const app = createSSRApp(App)
+  return {
+    app,
+  }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,58 @@
 import { defineConfig } from 'vite'
 import copy from 'rollup-plugin-copy';
+import { fileURLToPath, URL } from 'node:url'
 import uni from '@dcloudio/vite-plugin-uni'
-import Unocss from 'unocss/vite';
-import AutoImport from 'unplugin-auto-import/vite';
-import Inspect from 'vite-plugin-inspect';
-import { UnocssToUni } from 'vite-plugin-unocss-to-uni';
+import Unocss from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { UnocssToUni } from 'vite-plugin-unocss-to-uni'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        uni(),
+  plugins: [
+    uni(),
+    Unocss(),
 
-        Unocss(),
+    UnocssToUni(),
 
-        UnocssToUni(),
-
-        AutoImport({
-            imports: [
-                'vue',
-                // 'vue/macros',
-                // 'pinia',
-                // '@vueuse/core',
-            ],
-            dts: 'src/auto-imports.d.ts',
-        }),
-
-        copy({
-            copyOnce: process.env.NODE_ENV !== 'production', // 开发环境只复制一次
-            targets: [
-                {
-                    src: `src/susceptor/${process.env.UNI_PLATFORM}`,
-                    dest: `dist/${process.env.NODE_ENV === 'production' ? 'build' : 'dev'}`,
-                },
-            ],
-        }),
-
-        Inspect(),
-    ],
+    AutoImport({
+      imports: [
+        'vue',
+        // 'pinia',
+      ],
+      dts: 'src/auto-imports.d.ts',
+    }),
+    copy({
+      copyOnce: process.env.NODE_ENV !== 'production', // 开发环境只复制一次
+      targets: [
+        {
+          src: `src/susceptor/${process.env.UNI_PLATFORM}`,
+          dest: `dist/${
+            process.env.NODE_ENV === 'production' ? 'build' : 'dev'
+          }`,
+        },
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '#': fileURLToPath(new URL('./types', import.meta.url)),
+    },
+  },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        keep_infinity: true,
+        // drop_console: false,
+        // pure_funcs: ['console.log'],
+      },
+    },
+    // Turning off brotliSize display can slightly reduce packaging time
+    reportCompressedSize: false,
+    chunkSizeWarningLimit: 2000,
+  },
 })
+
+
+
